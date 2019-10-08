7d5439036b343923ec3156613324f4bad56a2799
Lumorti <lbmortimer1998@gmail.com>
2019-10-08 23:48:36 +0100

    made it more music-y

--- server.py
+++ server.py
@@ -32,11 +32,11 @@ hostip = get_ip()
 
 # Song options DEBUG
 options = []
-options.append({"title": "skip song", "artist": ""})
-options.append({"title": "refresh choices", "artist": ""})
 options.append({"title": "All Star", "artist": "Smash Mouth"})
 options.append({"title": "Mr Brightside", "artist": "The Killers"})
 options.append({"title": "Bohemian Rhapsody", "artist": "Queen"})
+options.append({"title": "Don't Stop Me Now", "artist": "Queen"})
+options.append({"title": "Moonage Daydream", "artist": "David Bowie"})
 
 # Set up the server
 listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -52,65 +52,16 @@ with open("client.html") as f:
 with open("screen.html") as f:
     screenPage = okResponse + f.read()
     screenPage = screenPage.encode("utf-8")
-with open("questions.json") as f:
-    questions = json.load(f)["data"]
-with open("fields.json") as f:
-    fields = json.load(f)
-if path.exists("saved_responses.json"):
-    with open("saved_responses.json") as f:
-        responses = json.load(f)
-if path.exists("saved_fields.json"):
-    with open("saved_fields.json") as f:
-        fields = json.load(f)
 
-# Save the answers given so far to a file 
-def saveAnswers():
-    with open("saved_responses.json", "w+") as f:
-        json.dump(responses, f)
-    with open("saved_fields.json", "w+") as f:
-        json.dump(fields, f)
+# Spotipy setup TODO 1
+def setupSpot():
 
-# Generate a question TODO 1
-def getQuestion():
+    pass
 
-    index = random.randint(0,len(questions)-1)
+# Get a set of five choices TODO 1
+def getChoices():
 
-    q = questions[index]["text"]
-
-    # See if any words need replacing e.g. $(NAME)
-    inWord = False
-    word = ""
-    i = 0
-    while i < len(q):
-
-        if q[i] == "$":
-            inWord = True
-
-        elif q[i] == ")" and inWord:
-            inWord = False
-
-            # Determine the word to replace it with
-            newWord = "ERROR"
-            for field in list(fields.keys()):
-                if field == word:
-                    newIndex = random.randint(0, len(fields[field])-1)
-                    newWord = fields[field][newIndex]
-
-            q = q[:i-len(word)-2] + "<span>" + newWord + "</span>" + q[i+1:]
-            q = q.replace("  ", " ")
-            q = q.replace(" :", ":")
-            q = q.replace(" ?", "?")
-            q = q.replace(" ,", ",")
-            i = -1
-
-            word = ""
-
-        elif inWord and q[i] != "(":
-            word += q[i]
-
-        i += 1
-
-    return q, index
+    pass
 
 # Main server loop
 while True:
@@ -136,23 +87,6 @@ while True:
                 responseType = "screen"
             elif "/refresh" in line:
                 responseType = "refresh"
-            elif "/send" in line:
-                answer = line[line.find("/send")+5:line.find("HTTP")]
-                answer = answer.replace("%20", " ")
-                answer = answer.strip()
-                ques = questionsPerIP[str(client_address[0])]
-                if ques[0:4] == "Give":
-                    field = ""
-                    for q in questions:
-                        if q["type"] == "request" and q["text"][0:15] == ques[0:15]:
-                            field = q["field"]
-                            break
-                    fields[field].append(answer)
-                else:
-                    responses.append({"question": ques, "answer": answer, "laughs":0, "scares":0,"up":0,"down":0})
-                saveAnswers()
-            elif "/answer" in line:
-                responseType = "answer"
             elif "/info" in line:
                 responseType = "info"
             elif "/ " in line:
@@ -171,18 +105,7 @@ while True:
             response = clientPage
         elif responseType == "refresh":
             response = textResponse
-            q, i = getQuestion()
-            response += q
-            response = response.encode("utf-8")
-            questionsPerIP[str(client_address[0])] = q
-        elif responseType == "answer":
-            response = textResponse
-            #TODO 1 choose best response
-
-            # If any have been shown less than twice, show them
-
-            # Otherwise pick randomly
-
+            response += json.dumps(options).replace("'", "#x420")
             response = response.encode("utf-8")
         elif responseType == "info":
             response = textResponse
